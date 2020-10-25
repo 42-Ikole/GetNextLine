6b8eb6c519d54de06d354c7ca8e62893a7d4c40a
Ingmar Kole <ikole@student.codam.nl>
2020-10-25 12:27:51 +0100

    Go get those lines!

---
+++ iterative/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ikole <ikole@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/01/10 09:45:35 by ikole          #+#    #+#                */
+/*   Updated: 2020/01/14 10:58:45 by ikole         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 128
+# endif
+
+int get_next_line(int fd, char **line);
+
+#endif
